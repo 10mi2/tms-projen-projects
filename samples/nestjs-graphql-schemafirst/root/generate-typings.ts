85fc433210631a0d2cdae78be5ac87c2dd2a4cff
Rob Giseburt <giseburt@gmail.com>
2023-12-05 13:53:27 -0600

    fix(esm-support): `generate-typings.ts` wouldn't run with ESM support was disabled

--- samples/nestjs-graphql-schemafirst/root/generate-typings.ts
+++ samples/nestjs-graphql-schemafirst/root/generate-typings.ts
@@ -2,8 +2,12 @@ import { join } from "path";
 import { GraphQLDefinitionsFactory } from "@nestjs/graphql";
 
 const definitionsFactory = new GraphQLDefinitionsFactory();
-await definitionsFactory.generate({
-  typePaths: ["./src/**/*.graphql"],
-  path: join(process.cwd(), "src/graphql.schema.ts"),
-  outputAs: "interface",
-});
+// if esmSupportConfig is `true` then `void` can be `await` and the
+// `.then(() => void 0)` can be removed.
+void definitionsFactory
+  .generate({
+    typePaths: ["./src/**/*.graphql"],
+    path: join(process.cwd(), "src/graphql.schema.ts"),
+    outputAs: "interface",
+  })
+  .then(() => void 0);
